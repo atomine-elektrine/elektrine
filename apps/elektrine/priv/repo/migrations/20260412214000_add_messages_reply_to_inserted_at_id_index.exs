c4aca63ac72b8f55526db68a1409a4f014d206b8
Maxfield Luke <maxfield@elektrine.com>
2026-04-12 18:36:15 -0400

    fix timelien infinite scroll behavior

--- apps/elektrine/priv/repo/migrations/20260412214000_add_messages_reply_to_inserted_at_id_index.exs
+++ apps/elektrine/priv/repo/migrations/20260412214000_add_messages_reply_to_inserted_at_id_index.exs
@@ -1,9 +1,13 @@
 defmodule Elektrine.Repo.Migrations.AddMessagesReplyToInsertedAtIdIndex do
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+
   def change do
     create_if_not_exists index(:messages, [:reply_to_id, :inserted_at, :id],
-                           name: :messages_reply_to_id_inserted_at_id_idx
+                           name: :messages_reply_to_id_inserted_at_id_idx,
+                           concurrently: true,
+                           where: "reply_to_id IS NOT NULL"
                          )
   end
 end
