63172875debb727a27626a871104ebc0dea40eb4
Maxfield Luke <maxfield@elektrine.com>
2026-04-11 21:21:55 -0400

    updates

--- apps/elektrine_chat/mix.exs
+++ apps/elektrine_chat/mix.exs
@@ -9,7 +9,7 @@ defmodule ElektrineChat.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.14",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(),
       start_permanent: Mix.env() == :prod,
@@ -43,9 +43,9 @@ defmodule ElektrineChat.MixProject do
     [
       internal_dep(:elektrine),
       internal_dep(:elektrine_web),
-      {:phoenix, "== 1.8.3"},
+      {:phoenix, "== 1.8.5"},
       {:phoenix_html, "== 4.3.0"},
-      {:phoenix_live_view, "== 1.1.26"},
+      {:phoenix_live_view, "== 1.1.28"},
       {:jason, "== 1.4.4"}
     ]
   end
--- apps/elektrine_password_manager/mix.exs
+++ apps/elektrine_password_manager/mix.exs
@@ -9,7 +9,7 @@ defmodule ElektrinePasswordManager.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.14",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(),
       start_permanent: Mix.env() == :prod,
@@ -37,9 +37,9 @@ defmodule ElektrinePasswordManager.MixProject do
   defp deps do
     [
       internal_dep(:elektrine),
-      {:phoenix, "== 1.8.3"},
+      {:phoenix, "== 1.8.5"},
       {:phoenix_html, "== 4.3.0"},
-      {:phoenix_live_view, "== 1.1.26"},
+      {:phoenix_live_view, "== 1.1.28"},
       {:jason, "== 1.4.4"}
     ]
   end
