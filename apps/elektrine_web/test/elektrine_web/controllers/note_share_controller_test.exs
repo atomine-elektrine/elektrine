3072bafa744a450f10f0f5c8f20d74e6b6983251
Maxfield Luke <maxfield@elektrine.com>
2026-06-24 04:20:24 -0400

    Escape note share JSON script payloads

--- apps/elektrine_web/test/elektrine_web/controllers/note_share_controller_test.exs
+++ apps/elektrine_web/test/elektrine_web/controllers/note_share_controller_test.exs
@@ -25,4 +25,23 @@ defmodule ElektrineWeb.NoteShareControllerTest do
 
     assert response(conn, 404) == "Not found"
   end
+
+  test "escapes encrypted payload JSON inside script data blocks", %{conn: conn} do
+    user = AccountsFixtures.user_fixture()
+    {:ok, note} = Notes.create_note(user.id, %{title: "Encrypted", body: "secret"})
+
+    {:ok, share} =
+      Notes.create_encrypted_note_share(user.id, note, %{
+        "version" => "v1",
+        "algorithm" => "aes-gcm",
+        "iv" => "iv",
+        "ciphertext" => "</script><script>alert(1)</script>"
+      })
+
+    conn = get(conn, ~p"/notes/share/#{share.token}")
+    html = html_response(conn, 200)
+
+    refute html =~ "</script><script>alert(1)</script>"
+    assert html =~ "\\u003C/script\\u003E\\u003Cscript\\u003Ealert(1)\\u003C/script\\u003E"
+  end
 end
