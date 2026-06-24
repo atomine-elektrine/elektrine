5401a933b3e17e45cd122dfbeb221e43c75db06f
Maxfield Luke <maxfield@elektrine.com>
2026-06-24 04:20:24 -0400

    Escape note share JSON script payloads

--- apps/elektrine_web/lib/elektrine_web/controllers/note_share_html.ex
+++ apps/elektrine_web/lib/elektrine_web/controllers/note_share_html.ex
@@ -2,4 +2,19 @@ defmodule ElektrineWeb.NoteShareHTML do
   use ElektrineWeb, :html
 
   embed_templates "note_share_html/*"
+
+  def script_json(value) do
+    value
+    |> Jason.encode!()
+    |> escape_script_data()
+  end
+
+  defp escape_script_data(json) when is_binary(json) do
+    json
+    |> String.replace("<", "\\u003C")
+    |> String.replace(">", "\\u003E")
+    |> String.replace("&", "\\u0026")
+    |> String.replace("\u2028", "\\u2028")
+    |> String.replace("\u2029", "\\u2029")
+  end
 end
